d631228be77651571adde2aebb73ee1d1dd47111
Dominik277 <dominikdoric277@gmail.com>
2021-01-01 16:28:50 +0100

    Dodani elementi u spinner.

--- app/src/main/java/beer/adviser/MainActivity.java
+++ app/src/main/java/beer/adviser/MainActivity.java
@@ -1,8 +1,9 @@
 package beer.adviser;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //array unutar kojeg smo spremili elemente spinnera
+        String[] arraySpinner = new String[]{"light","amber","brown","dark"};
+        //stvorili spinner objekt od XML elementa
+        Spinner spinner = findViewById(R.id.spinner);
+
+        //adapter objekt koji nam sluzi za spremanje informacija kako cemo prikazati
+        //elemente unutar spinnera i koje podatke cemo prikazati u spinner-u
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_list_item_1,arraySpinner);
+
+        //ovako govorimo adapteru kako ce izgledati drop down lista kao argument
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_list_item_1);
+
+        //ovdje upogonjujemo taj adapter unutar spinnera
+        spinner.setAdapter(arrayAdapter);
     }
 }
