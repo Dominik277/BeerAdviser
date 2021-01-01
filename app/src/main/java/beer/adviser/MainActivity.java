e9a0dae8a120849fe6634f4237c34eeeb97abbdb
Dominik277 <dominikdoric277@gmail.com>
2021-01-01 23:25:18 +0100

    Zavrseno sve, jedino jos objasnjenja dodati.

--- app/src/main/java/beer/adviser/MainActivity.java
+++ app/src/main/java/beer/adviser/MainActivity.java
@@ -3,57 +3,35 @@ package beer.adviser;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
+    BeerExpert expert = new BeerExpert();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Spinner spinner = findViewById(R.id.spinner);
-        Button button = findViewById(R.id.find_beer);
-        TextView textView = findViewById(R.id.brands);
-
-        String[] arraySpinner = new String[]{"light","amber","brown","dark"};
-        String[] versionsOfBeer = new String[]{"Ozujsko","Karlovacko","Pan","Osjecko"};
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
-                this, android.R.layout.simple_list_item_1,arraySpinner);
-        arrayAdapter.setDropDownViewResource(android.R.layout.simple_list_item_1);
-        spinner.setAdapter(arrayAdapter);
-
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String text = spinner.getSelectedItem().toString();
-
-                button.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        if (text == arraySpinner[0]){
-                            textView.setText(versionsOfBeer[0]);
-                        }else if (text == arraySpinner[1]){
-                            textView.setText(versionsOfBeer[1]);
-                        }else if (text == arraySpinner[2]){
-                            textView.setText(versionsOfBeer[2]);
-                        }else if (text == arraySpinner[3]){
-                            textView.setText(versionsOfBeer[3]);
-                        }
-                    }
-                });
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
-
     }
+
+    public void onClickFindBeer(View view){
+        TextView brands = findViewById(R.id.brands);
+        Spinner color = findViewById(R.id.color);
+
+        String beerType = (String) color.getSelectedItem();
+        List<String> brandsList = expert.getBrands(beerType);
+        StringBuilder brandsFormatted = new StringBuilder();
+
+        for (String brand:brandsList){
+            brandsFormatted.append(brand).append("\n");
+        }
+        brands.setText(brandsFormatted);
+    }
+
 }
