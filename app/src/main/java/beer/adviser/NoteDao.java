34fa70e72f544d92d57d9cb13a0db608c6985345
Dominik277 <dominikdoric277@gmail.com>
2021-01-27 22:17:58 +0100

    Kreiran DAO interfejs.

---
+++ app/src/main/java/beer/adviser/NoteDao.java
@@ -0,0 +1,31 @@
+package beer.adviser;
+
+import android.provider.SyncStateContract;
+
+import java.util.List;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+@Dao
+public interface NoteDao {
+
+    @Query("SELECT * FROM note")
+    List<Note> getAll();
+
+    @Insert
+    void insert(Note note);
+
+    @Update
+    void update(Note repos);
+
+    @Delete
+    void delete(Note note);
+
+    @Delete
+    void delete(Note... note);
+
+}
