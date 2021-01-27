90fb0e49b9503093bec8e64b13198b9afe45218f
Dominik277 <dominikdoric277@gmail.com>
2021-01-27 22:12:41 +0100

    Dodane jos tri metode.

--- app/src/main/java/beer/adviser/Note.java
+++ app/src/main/java/beer/adviser/Note.java
@@ -47,4 +47,33 @@ public class Note {
         this.title = title;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Note)) return false;
+
+        Note note = (Note) o;
+
+        if (note_id != note.note_id) return false;
+        return title != null ? title.equals(note.title) : note.title == null;
+    }
+
+
+
+    @Override
+    public int hashCode() {
+        int result = note_id;
+        result = 31 * result + (title != null ? title.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Note{" +
+                "note_id=" + note_id +
+                ", content='" + content + '\'' +
+                ", title='" + title + '\'' +
+                '}';
+    }
+
 }
