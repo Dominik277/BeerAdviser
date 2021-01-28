d3c0c7ec8903da84f29e68de6078f6eaa6427ab8
Dominik277 <dominikdoric277@gmail.com>
2021-01-28 19:28:29 +0100

    Konstruirana klasa NotesAdapter koja ce sluziti kao adapter za RecyclerView.

--- app/src/main/java/beer/adviser/adapter/NotesAdapter.java
+++ app/src/main/java/beer/adviser/adapter/NotesAdapter.java
@@ -1,4 +1,40 @@
 package beer.adviser.adapter;
 
-public class NotesAdapter {
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.BeanHolder> {
+
+
+    @NonNull
+    @Override
+    public NotesAdapter.BeanHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NotesAdapter.BeanHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class BeanHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+        public BeanHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+
+        @Override
+        public void onClick(View v) {
+
+        }
+    }
+
 }
