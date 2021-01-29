1cea5ed70afdf293b3e9478a17d781a4dc19584d
Dominik277 <dominikdoric277@gmail.com>
2021-01-29 12:20:24 +0100

    Dodana logika u metode za RecyclerView.

--- app/src/main/java/beer/adviser/adapter/NotesAdapter.java
+++ app/src/main/java/beer/adviser/adapter/NotesAdapter.java
@@ -1,28 +1,49 @@
 package beer.adviser.adapter;
 
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.List;
+
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+import beer.adviser.R;
+import beer.adviser.notedb.Note;
 
 public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.BeanHolder> {
 
+    private List<Note> list;
+    private Context context;
+    private LayoutInflater layoutInflater;
+    private OnNoteItemClick onNoteItemClick;
+
+    public NotesAdapter(List<Note> list,Context context){
+        layoutInflater = LayoutInflater.from(context);
+        this.list = list;
+        this.context = context;
+        this.onNoteItemClick = (OnNoteItemClick) context;
+    }
 
     @NonNull
     @Override
     public NotesAdapter.BeanHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = layoutInflater.inflate(R.layout.note_list_item,parent,false);
+        return new BeanHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull NotesAdapter.BeanHolder holder, int position) {
-
+        Log.e("bind","onBindViewHolder" + list.get(position));
+        holder.textViewTitle.setText(list.get(position).getTitle());
+        holder.textViewContent.setText(list.get(position).getContent());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return list.size();
     }
 
     public class BeanHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
